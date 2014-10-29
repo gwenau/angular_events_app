39991a5802ed6a41a20342f61a9178dc55011f5b
Gwen Au <quyen_au@hotmail.com>
2014-10-29 20:46:47 +0000

    Only add to favourite function works at the moment.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,21 @@
 
   app.controller('eventsController' , ["$scope","$http", function($scope, $http) {
 
-    $http.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyBqoZ-SBdyqfayjnfKX0XPvl55b6R1NK20&cx=003846610913805178311:nnrjjbpd8q8&q=events&alt=json").success(function(data){
+    $scope.eventSearch = "";
+    $scope.interestedList = [];
+
+    $scope.searchEvents = function(){
+      $http.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyBqoZ-SBdyqfayjnfKX0XPvl55b6R1NK20&cx=003846610913805178311:nnrjjbpd8q8&q="+$scope.eventSearch+"&alt=json").success(function(data){
+      $scope.eventList = true;
       $scope.jsons = data["items"];
-      console.log($scope.jsons)
       })
+    }
+
+    $scope.interested = function(json){
+      $scope.interestedList.push(
+      json);
+      console.log($scope.interestedList);
+    }
 
   }])
 
